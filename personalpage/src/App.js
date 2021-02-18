3b2235a2c85eaf06e79ee0e1932e79a7cf00c091
Deric_Parra <parradekoc@gmail.com>
2021-02-18 10:40:37 -0300

    base react

---
+++ personalpage/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hero from './components/Hero';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      <Cards />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ personalpage/src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Avatar from './Avatar';
+import Contact from './Contact';
+
+const Hero = () => {
+  return (
+    <div>
+      <Avatar />
+      <h1>Hi Im Deko Web Developer</h1>
+      <p>Front end Developer | Html | Css | Js | React</p>
+      <Contact />
+    </div>
+  );
+};
+
+export default Hero;
